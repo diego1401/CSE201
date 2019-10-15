36607ea5af6dbe5487f14e1d241c28af19cf78dd
diego1401 <diegoandre.gomez@gmail.com>
2019-10-15 21:41:25 +0200

    update

---
+++ TD4/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void greetingPalaiseau() {
+  std::cout << "Hello Palaiseau!" << std::endl;
+}
+
+int main() {
+  greetingPalaiseau();
+  std::cout << "Hello Ecole Polytechnique!" << std::endl;
+}
+
+void greetingPalaiseau() {
+  std::cout << "Hello Palaiseau!" << std::endl;
+}
